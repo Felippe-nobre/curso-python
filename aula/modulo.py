3b7661342c6b15c9d93cd990142ad5a6693c54c3
Felippe Nobre <113641116+Felippe-nobre@users.noreply.github.com>
2024-10-22 15:05:58 -0300

    novas aulas

---
+++ aula/modulo.py
@@ -0,0 +1,15 @@
+#Módulos são arquivos que contêm definições e implementações de funções, classes e variáveis que você pode reutilizar em diferentes partes do seu código. Python possui muitos módulos padrão que você pode importar para ampliar suas funcionalidades, sem ter que reinventar a roda.
+
+import math#Para importar um módulo, usamos a palavra-chave import. Por exemplo, o módulo math contém funções matemáticas avançadas:
+print(math.sqrt(16))
+
+from math import pi#Você também pode importar apenas uma parte do módulo usando from ... import ....
+print(pi)
+
+import math as m#Você pode usar a palavra-chave as para renomear um módulo ou função para simplificar seu uso.
+print(m.sqrt(25))
+
+
+import meu_modulo   #Assim como o Python oferece módulos, você também pode criar os seus. Basta criar um arquivo .py com funções e variáveis que deseja reutilizar.
+print(meu_modulo.saudacao("Felippe"))
+
