ddc2b24e15d980607114e17f79bfd414ba4d6397
Felippe Nobre <113641116+Felippe-nobre@users.noreply.github.com>
2024-10-24 00:38:32 -0300

    .

--- aula/Manipulação-de-Arquivos.py
+++ aula/Manipulação-de-Arquivos.py
@@ -11,11 +11,11 @@ conteudo = arquivo.read()
 print(conteudo)  # Saída: Olá, Python!
 arquivo.close()
 
-
 with open("Exemplo.txt", "r") as arquivo: 
 # Usando `with` para manipulação de arquivos. A palavra-chave `with` permite abrir e fechar arquivos de forma automática, tornando o código mais limpo e seguro.
     conteudo = arquivo.read()
     print(conteudo)
+
 
 #Arquivos CSV (Comma-Separated Values) são comumente usados para armazenar dados tabulares. Python possui o módulo csv para facilitar a leitura e escrita desses arquivos.
 #Lendo um arquivo CSV
@@ -31,3 +31,29 @@ dados = [["Nome", "Idade"], ["Ana", 28], ["Carlos", 35]]
 with open("dados.csv", "W", newline="") as arquivo_csv:
     escritor = csv.writer(arquivo_csv)
     escritor.writerows(dados)
+
+
+#O JSON (JavaScript Object Notation) é um formato de armazenamento de dados muito utilizado para comunicação entre sistemas. Python possui o módulo json para manipular esses arquivos
+#Escrevendo em um arquivo JSON
+import json
+dados = {"nome": "Ana", "idade": 28, "cidade": "São Paulo"}
+
+with open("dados.json", "w") as arquivo:
+    json.dump(dados, arquivo)
+#Lendo um arquivo JSON
+import json
+with open("dados.json", "r") as arquivo_json:
+    dados = json.load(arquivo_json)
+    print(dados)
+#Analogia: O JSON é como uma receita de culinária 🍴, onde as informações são armazenadas de forma estruturada e fácil de entender.
+
+#Trabalhando com arquivos binários
+#Além de arquivos de texto e CSV, você pode manipular arquivos binários, como imagens, arquivos executáveis, entre outros.
+
+with open("copia_imagem.png", "wb") as arquivo_binario:
+    arquivo_binario.write(conteudo)
+
+with open("imagem.png", "rb") as arquivo_binario:
+    conteudo = arquivo_binario.read()
+    print(conteudo)
+
